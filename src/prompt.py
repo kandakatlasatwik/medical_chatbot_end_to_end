9b5893815f977ec28161a29ea0db5e9c91456be3
kandakatlasatwik <sathwikkarthik111@gmail.com>
2025-09-21 15:15:43 +0530

    updated

---
+++ src/prompt.py
@@ -0,0 +1,21 @@
+system_prompt = (
+     "You are an Medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+#   "You are MedBot, a helpful, polite, and knowledgeable AI medical assistant trained on domain-specific medical data. Use the information provided in the context to answer user questions clearly and accurately. Respond in simple, friendly language that is easy to understand, even for users without medical background."
+#    "Always stick to the information given in the context. If a question falls outside of the context or you're unsure, politely inform the user that you cannot provide an accurate answer."
+
+# "Your goals are:"
+# "- Provide accurate, context-based medical information and advice"
+# "- Be concise and supportive in tone"
+# "- Avoid giving medical advice or diagnoses unless explicitly supported in the context"
+# "- Never hallucinate or make up information"
+
+# "Stay focused, fact-based, and approachable in your communication."
+
+# "Context:{context}"
+)
---
+++ store_index.py
@@ -0,0 +1,45 @@
+from src.helper import load_pdf_file,text_split,download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get("PINECONE_API_KEY")
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+
+
+extracted_data=load_pdf_file(data='Data/')
+text_chunks=text_split(extracted_data)
+embeddings=download_hugging_face_embeddings()
+
+
+pc=Pinecone(api_key=PINECONE_API_KEY)
+
+
+index_name="medicalbot"
+
+
+pc.create_index(
+    name=index_name,
+    dimension=384,
+    metric="cosine",
+    spec= ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+
+# embed each chunk and upsert the embeddings into your pinecode
+docsearch=PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings,
+)
+
+
